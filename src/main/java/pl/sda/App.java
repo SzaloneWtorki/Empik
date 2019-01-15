c4c6c244011f86e626804ba355b944ef0d5259ee
tornadusNEWB <nowaczek5@o2.pl>
2019-01-15 20:50:34 +0100

    new empik

--- src/main/java/pl/sda/App.java
+++ src/main/java/pl/sda/App.java
@@ -10,10 +10,12 @@ public class App {
         Basket basket = new Basket();
         basket.add(new Book(1,"ksiazka", 100,140));
         basket.add(new Cd(2,"CD", 100,2000));
-        basket.summary();
+
 
         basket.add((new Poster(1,"poster1",666.5)));
         basket.add((new Poster(2,"poster2",666)));
         basket.add((new Poster(3,"poster3",111)));
+
+        basket.summary();
     }
 }
--- src/main/java/pl/sda/service/Basket.java
+++ src/main/java/pl/sda/service/Basket.java
@@ -35,24 +35,33 @@ public class Basket {
     }
 
     public void summary() {
+        pricePoster();
         System.out.printf("%3s | %-10s | %5s | %20s", "id", "name", "price", "price after discount\n");
         for (int i = 0; i < basket.size(); i++) {
             System.out.printf("%3s | %-10s | %5s | %20s\n", basket.get(i).getId(), basket.get(i).getName(), basket.get(i).getPrice(), basket.get(i).discount());
         }
         System.out.println(priceSum());
+
     }
 
-    public void posterDiscount() {
+    public void pricePoster() {
         int numberOfPosters = 0;
-        double theLowestPrice = 0;
+        double lowestPrice = 0.0;
         for (int i = 0; i < basket.size(); i++) {
             if (basket.get(i) instanceof Poster) {
                 numberOfPosters++;
-                    if (basket.get(i).getPrice() < theLowestPrice) {
-                        theLowestPrice = basket.get(i).getPrice();
-                    }
+                if (basket.get(i).getPrice() < lowestPrice || lowestPrice == 0.0) {
+                    lowestPrice = basket.get(i).getPrice();
                 }
             }
-            
+        }
+        if (numberOfPosters >= 3) {
+            for (int i = 0; i < basket.size(); i++) {
+                if (basket.get(i) instanceof Poster && basket.get(i).getPrice() == lowestPrice) {
+                    ((Poster) basket.get(i)).setDiscount(true);
+                    return;
+                }
+            }
         }
     }
+}
